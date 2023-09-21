a14e4e48c137acaa046aeac907bf9e35f703d475
Matthew Truscott <matthew.a.truscott@gmail.com>
2023-09-21 07:46:55 +0100

    custom

--- init.lua
+++ init.lua
@@ -65,6 +65,25 @@ return {
     },
   },
 
+  plugins = {
+    { -- override nvim-cmp plugin
+      "hrsh7th/nvim-cmp",
+      -- override the options table that is used in the `require("cmp").setup()` call
+      opts = function(_, opts)
+        -- opts parameter is the default options table
+        -- the function is lazy loaded so cmp is able to be required
+        local cmp = require "cmp"
+        -- modify the mapping part of the table
+        opts.mapping["<A-j>"] = cmp.mapping.select_next_item()
+        opts.mapping["<A-k>"] = cmp.mapping.select_prev_item()
+        opts.mapping["<A-l>"] = cmp.mapping.confirm()
+
+        -- return the new table to be used
+        return opts
+      end,
+    },
+  },
+
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
